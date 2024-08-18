5550770d192fd015de60d3c5c311910a6e008939
dbwhdtjr0457 <dbwhdtjr0457@gmail.com>
2024-08-18 20:43:04 +0900

    Feat: Component: 많은 div들을 한 단어로 줄이고 싶으면

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [titles, setTitles] = useState([
+    "남자 코트 추천",
+    "강남 우동 맛집",
+    "파이썬 독학",
+  ]);
+
+  const listItems = titles.map((title) => (
+    <div className="list" key={title}>
+      <h4>{title}</h4>
+      <p>2월 17일 발행</p>
+    </div>
+  ));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 blog</div>
+      </div>
+      <div>{listItems}</div>
+      <button
+        onClick={() => {
+          let copy = [...titles];
+          copy[0] = "여자 코트 추천";
+          setTitles(copy);
+        }}
+      >
+        수정버튼
+      </button>
     </div>
   );
 }
