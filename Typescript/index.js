9bf5adbb85ce9044782d76400d8b0254b9c2cc4e
dbwhdtjr0457 <dbwhdtjr0457@khu.ac.kr>
2024-09-03 16:58:36 +0900

    타입 미리 정하기 애매할 때 (typescript union type, any, unknown)

--- Typescript/index.js
+++ Typescript/index.js
@@ -1,13 +1,29 @@
-var username = "kim";
-var age = 50;
-var marriage = true;
-var members = ["kim", "park"];
-// 온갖 곳에 다 타입 지정해놓을 필요는 없음
-// 왜냐면... 알아서 타입 지정을 해줌!
-// let temp = "string";
-// temp = 3; -> Type 'number' is not assignable to type 'string'.
-var project = {
-    member: ["kim", "park"],
-    days: 30,
-    started: true,
+// Union Type -> 타입 2개 이상 합친 새로운 타입 만들기
+var 회원 = 123;
+var 회원들 = [1, "2", 3];
+// any, unknown
+var 이름 = "123";
+var 이름2 = "123";
+// unknown이 any보다는 안전함
+// let 변수1 :string = 이름 이런 걸 했을 때
+// 이름이 any면 에러가 안 나지만 unknown이면 에러 발생
+// 혹은 let 이름: unknown일 때
+// 이름 - 1 이런 걸 못함
+// let 나이: string | number;
+// 나이 + 1;
+// -> string | number라는 타입이기 때문에 string도 아니고 number도 아님 -> 에러!
+// let 나이: unknown = 1;
+// 나이 - 1;
+// -> unknown 타입이기 때문에 에러! number가 들어가있다고 해도 에러!
+// 숙제
+var user = "kim";
+var age = undefined;
+var married = false;
+var 철수 = [user, age, married];
+var 학교 = {
+    score: [100, 97, 84],
+    teacher: "Phil",
+    friend: "John",
 };
+학교.score[4] = false;
+학교.friend = ["Lee", 학교.teacher];
--- Typescript/index.ts
+++ Typescript/index.ts
@@ -1,22 +1,41 @@
-let username: string = "kim";
+// Union Type -> 타입 2개 이상 합친 새로운 타입 만들기
+let 회원: number | string = 123;
 
-let age: number = 50;
+let 회원들: (number | string)[] = [1, "2", 3];
 
-let marriage: boolean = true;
+// any, unknown
+let 이름: any = "123";
+let 이름2: unknown = "123";
 
-let members: string[] = ["kim", "park"];
+// unknown이 any보다는 안전함
+// let 변수1 :string = 이름 이런 걸 했을 때
+// 이름이 any면 에러가 안 나지만 unknown이면 에러 발생
+// 혹은 let 이름: unknown일 때
+// 이름 - 1 이런 걸 못함
 
-// 온갖 곳에 다 타입 지정해놓을 필요는 없음
-// 왜냐면... 알아서 타입 지정을 해줌!
-// let temp = "string";
-// temp = 3; -> Type 'number' is not assignable to type 'string'.
+// let 나이: string | number;
+// 나이 + 1;
+// -> string | number라는 타입이기 때문에 string도 아니고 number도 아님 -> 에러!
 
-let project: {
-  member: string[];
-  days: number;
-  started: boolean;
+// let 나이: unknown = 1;
+// 나이 - 1;
+// -> unknown 타입이기 때문에 에러! number가 들어가있다고 해도 에러!
+
+// 숙제
+let user: string = "kim";
+let age: undefined | number = undefined;
+let married: boolean = false;
+let 철수: (string | number | undefined | boolean)[] = [user, age, married];
+
+let 학교: {
+  score: (number | boolean)[];
+  teacher: string;
+  friend: string | string[];
 } = {
-  member: ["kim", "park"],
-  days: 30,
-  started: true,
+  score: [100, 97, 84],
+  teacher: "Phil",
+  friend: "John",
 };
+
+학교.score[4] = false;
+학교.friend = ["Lee", 학교.teacher];
