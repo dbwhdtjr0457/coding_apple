2213cc8abca4bb6d76d15737b7e68d429472e507
dbwhdtjr0457 <dbwhdtjr0457@khu.ac.kr>
2024-09-03 17:07:15 +0900

    함수에 타입 지정하는 법 & void 타입

--- Typescript/index.js
+++ Typescript/index.js
@@ -1,29 +1,21 @@
-// Union Type -> 타입 2개 이상 합친 새로운 타입 만들기
-var 회원 = 123;
-var 회원들 = [1, "2", 3];
-// any, unknown
-var 이름 = "123";
-var 이름2 = "123";
-// unknown이 any보다는 안전함
-// let 변수1 :string = 이름 이런 걸 했을 때
-// 이름이 any면 에러가 안 나지만 unknown이면 에러 발생
-// 혹은 let 이름: unknown일 때
-// 이름 - 1 이런 걸 못함
-// let 나이: string | number;
-// 나이 + 1;
-// -> string | number라는 타입이기 때문에 string도 아니고 number도 아님 -> 에러!
-// let 나이: unknown = 1;
-// 나이 - 1;
-// -> unknown 타입이기 때문에 에러! number가 들어가있다고 해도 에러!
-// 숙제
-var user = "kim";
-var age = undefined;
-var married = false;
-var 철수 = [user, age, married];
-var 학교 = {
-    score: [100, 97, 84],
-    teacher: "Phil",
-    friend: "John",
-};
-학교.score[4] = false;
-학교.friend = ["Lee", 학교.teacher];
+function 함수(x) {
+    return x * 2;
+}
+함수(6);
+// 혹은..
+function 함수2(x) {
+    console.log(x * 2);
+}
+함수2(3);
+// 만약 함수에 파라미터가 들어가야 된다고 적었다면
+// 함수를 호출할 때는 무조건 파라미터를 입력해야 함.
+// 함수2() -> 에러!
+// 들어오거나 안 들어오면 함수(x?: number)처럼 하면 됨
+// 함수(x?: number) == 함수(x: number | undefined)
+// 퀴즈
+// function 함수3(x: number | string): void {
+//   console.log(x + 3);
+// }
+// 함수3(2);
+// 에러가 나는 이유?
+// string | number는 string도 아니고 number도 아니기 때문에!
