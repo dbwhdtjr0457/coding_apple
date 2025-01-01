642466f32b982d577b13b379bd4af6e786b9f1bd
dbwhdtjr0457 <dbwhdtjr0457@gmail.com>
2025-01-01 21:46:28 +0900

    20250101: 함수와 methods에 type alias 지정하는 법

--- Typescript/index.js
+++ Typescript/index.js
@@ -1,16 +1,22 @@
-var animal;
-// -> 미리 정의하고 사용!
-var animal2 = "kim";
-var animal3 = { name: "kim", age: 123 };
-// type 이름은 첫 글자가 대문자인 것이 좋다.
-// + 타입 이름에는 Type 들어가는 게 좋기도 함
-var 출생지역 = { region: "seoul" };
-// const -> 재할당을 막아주는 놈이지, 객체 안의 내용 변경을 막는 놈이 아니다.
-출생지역.region = "busan";
-console.log(출생지역);
-var 여친 = {
-    name: "엠버",
+// 함수 타입 전체를 type으로 alias만들기
+// 이렇게 하면 함수 만들 때 타입 지정 직접 안 해도 된다.
+var 함수 = function (a) {
+    return 10;
 };
-var position = { x: 10, y: 20 };
-// type 재정의는 불가능!
-// type Animal = boolean -> 불가능!!! 이미 위에서 정의했기 때문!
+console.log(함수("hello"));
+var 회원정보 = {
+    name: "kim",
+    plusOne: function (a) {
+        return a + 1;
+    },
+    changeName: function () { },
+};
+회원정보.plusOne(10);
+var cutZero = function (x) {
+    var result = x.replace(/^0+/, "");
+    return result;
+};
+var removeDash = function (x) {
+    var result = x.replace(/-/g, "");
+    return parseFloat(result);
+};
--- Typescript/index.ts
+++ Typescript/index.ts
@@ -1,38 +1,43 @@
-let animal: string | number | undefined;
-// 굉장히 길다;;
+// 함수 타입 전체를 type으로 alias만들기
 
-type Animal = string | number | undefined;
-// -> 미리 정의하고 사용!
+type funcType = (a: string) => number;
 
-let animal2: Animal = "kim";
-// type alias 활용
+// 이렇게 하면 함수 만들 때 타입 지정 직접 안 해도 된다.
 
-type RealAnimal = { name: string; age: number };
-let animal3: RealAnimal = { name: "kim", age: 123 };
-// type 이름은 첫 글자가 대문자인 것이 좋다.
-// + 타입 이름에는 Type 들어가는 게 좋기도 함
-
-const 출생지역 = { region: "seoul" };
-// const -> 재할당을 막아주는 놈이지, 객체 안의 내용 변경을 막는 놈이 아니다.
-출생지역.region = "busan";
-console.log(출생지역);
-// {region: 'busan'}
-// 하지만 타입스크립트로 막을 수 있다.
-
-type Girlfriend = {
-  readonly name: string; // 읽기 전용으로 바꿔버리기
+let 함수: funcType = function (a) {
+  return 10;
 };
-const 여친: Girlfriend = {
-  name: "엠버",
+
+console.log(함수("hello"));
+
+// object 내 함수도 타입 지정 가능
+
+type Member = {
+  name: string;
+  plusOne: (a: number) => number;
+  changeName: () => void;
 };
-// 여친.name = "수지"; // 에러
 
-type PositionX = { x: number };
-type PositionY = { y: number };
+let 회원정보: Member = {
+  name: "kim",
+  plusOne(a) {
+    return a + 1;
+  },
+  changeName: () => {},
+};
 
-type NewType = PositionX & PositionY;
+회원정보.plusOne(10);
 
-let position: NewType = { x: 10, y: 20 };
+// 숙제
+type cutZeroFunction = (s: string) => string;
+type removeDashFunction = (s: string) => number;
 
-// type 재정의는 불가능!
-// type Animal = boolean -> 불가능!!! 이미 위에서 정의했기 때문!
+let cutZero: cutZeroFunction = function (x) {
+  let result = x.replace(/^0+/, "");
+  return result;
+};
+
+let removeDash: removeDashFunction = function (x) {
+  let result = x.replace(/-/g, "");
+  return parseFloat(result);
+};
