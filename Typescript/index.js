a350bc81f40b6a6e493e5f4fbd277a2027669b79
dbwhdtjr0457 <dbwhdtjr0457@gmail.com>
2025-01-06 20:30:04 +0900

    20250106: 함수 rest 파라미터, destructuring 할 떄 타입 지정

--- Typescript/index.js
+++ Typescript/index.js
@@ -1,10 +1,17 @@
-var 네모 = { color: "red", width: 100 };
-var 학생 = { name: "kim" };
-var 선생 = { name: "kim", age: 20 };
-var person = {
-    name: "kim",
-    age: 20,
-};
-// type은 불가능하다. strict하다.
-// 외부 라이브러리는 interface로 되어있는 경우가 많다. 확장 가능하게 설정하는 것이 가능하기 때문.
-// 즉! 다른 사람이 많이 이용할 것 같으면 interface 사용하자.
+// 함수 rest 파라미터, destructuring 할 떄 타입 지정
+function 함수1() {
+    var a = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        a[_i] = arguments[_i];
+    }
+    // -> 함수 rest 파라미터: ...a와 같이 몇 개 들어올지 모를 떄 사용
+    console.log(a);
+}
+함수1(1, 2, 3, 2, 1, 2);
+//quiz
+var person = { student: true, age: 20 };
+function 함수2(_a) {
+    var student = _a.student, age = _a.age;
+    console.log(student, age);
+}
+함수2(person);
--- Typescript/index.ts
+++ Typescript/index.ts
@@ -1,63 +1,22 @@
-// type 키워드도 사용 가능하지만, interface도 사용 가능.
-// type Square = { color: string; width: number };
-// let 네모: Square = { color: "red", width: 100 };
-//=>
-interface Square {
-  color: string;
-  width: number;
+// 함수 rest 파라미터, destructuring 할 떄 타입 지정
+function 함수1(...a: number[]) {
+  // -> 함수 rest 파라미터: ...a와 같이 몇 개 들어올지 모를 떄 사용
+  console.log(a);
 }
 
-let 네모: Square = { color: "red", width: 100 };
+함수1(1, 2, 3, 2, 1, 2);
 
-// type과 interface 차이점 배우기 전에...
-// quiz
-// interface Student {
-//   name: string;
-// }
+//quiz
 
-// interface Teacher {
-//   name: string;
-//   age: number;
-// }
+let person = { student: true, age: 20 };
 
-// let 학생: Student = { name: "kim" };
-
-// let 선생: Teacher = { name: "kim", age: 20 };
-
-// 하지만... 중복된다!
-// extends를 활용하면 기존 interface에 확장 가능.
-
-interface Student {
-  name: string;
-}
-
-interface Teacher extends Student {
+type Person = {
+  student: boolean;
   age: number;
-}
-
-let 학생: Student = { name: "kim" };
-let 선생: Teacher = { name: "kim", age: 20 };
-
-// type도 가능하긴 함
-// type Animal = { name: string };
-// type Cat = { age: number } & Animal;
-
-// type과 interface의 차이?
-// interface는 중복 선언이 가능하다. 자동 extend가 된다.
-
-interface Student {
-  name: string;
-}
-
-interface Student {
-  age: number;
-}
-
-let person: Student = {
-  name: "kim",
-  age: 20,
 };
 
-// type은 불가능하다. strict하다.
-// 외부 라이브러리는 interface로 되어있는 경우가 많다. 확장 가능하게 설정하는 것이 가능하기 때문.
-// 즉! 다른 사람이 많이 이용할 것 같으면 interface 사용하자.
+function 함수2({ student, age }: Person) {
+  console.log(student, age);
+}
+
+함수2(person);
